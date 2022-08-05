b07d7d51f91cefed2c729ca2e3d27d18909f0b90
zfone <zfonemore@foxmail.com>
2022-08-05 04:40:40 -0400

    change configs to add training params

--- configs/iai/ytvis2021_iai_condinst_r50.py
+++ configs/iai/ytvis2021_iai_condinst_r50.py
@@ -137,6 +137,20 @@ data = dict(
         test_mode=True,
         with_track=True))
 
+optimizer = dict(
+    type='AdamW',
+    lr=0.0001,
+    #lr=0.00005,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=0.9)}))
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
 runner = dict(type='EpochBasedRunner', max_epochs=12)
 checkpoint_config = dict(interval=1)
 log_config = dict(
